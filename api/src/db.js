c8029bfaed3843ceed01dbfee5c2e5c43be5b8fa
kripto-c <mgutierrezxred@gmail.com>
2022-09-22 13:04:36 -0400

    modificacion de db

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
         { logging: false, native: false }
       );
 
