9ba8588458584a09986fb71462f6259cf55cf337
kripto-c <mgutierrezxred@gmail.com>
2022-10-06 15:53:19 -0400

    modi card

--- client/src/Components/Cards/cards.jsx
+++ client/src/Components/Cards/cards.jsx
@@ -7,13 +7,13 @@ export default function Cards({breeds, link, onclick}) {
     return(
          <div className="Cards">
                {
-                 breeds && breeds.map(el=>{
+                 breeds.length > 0 ?  breeds.map(el=>{
                     return(
                       <>
                     <Card id={el.id}  name={el.name}  image={el.img} temperament={el.temperament} weigth_min={el.weight.min} weigth_max={el.weight.max} link={link} onclick={onclick ? onclick : false}/>
                       </>
                      );
-                  })
+                  }): <p>sin cards </p>
                }
          </div>
     );
