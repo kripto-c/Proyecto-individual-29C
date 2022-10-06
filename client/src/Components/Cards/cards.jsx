d154a3010062247d81883ae005b8f7ae274762a5
kripto-c <mgutierrezxred@gmail.com>
2022-10-06 15:26:22 -0400

    modifict card

--- client/src/Components/Cards/cards.jsx
+++ client/src/Components/Cards/cards.jsx
@@ -7,7 +7,7 @@ export default function Cards({breeds, link, onclick}) {
     return(
          <div className="Cards">
                {
-                  breeds?.map(el=>{
+                 breeds && breeds.map(el=>{
                     return(
                       <>
                     <Card id={el.id}  name={el.name}  image={el.img} temperament={el.temperament} weigth_min={el.weight.min} weigth_max={el.weight.max} link={link} onclick={onclick ? onclick : false}/>
